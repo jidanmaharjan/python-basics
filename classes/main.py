08a7579b436286995b3f23240d19ba8196d695cf
jidanmaharjan <jidanmaharjan1@gmail.com>
2025-06-09 17:36:47 +0000

    Classes

--- classes/main.py
+++ classes/main.py
@@ -1,1 +1,19 @@
-# 
+#class
+class MyClass:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print( f"Hello, {self.name}!")
+    
+# function
+test =  MyClass("World")
+
+test2 = MyClass("Python")
+
+test.greet()  # This will return "Hello, World!"
+
+test2.greet()  # This will return "Hello, Python!"
+
+    
+    
